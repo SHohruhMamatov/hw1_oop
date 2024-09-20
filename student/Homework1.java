7ea7d4a645f0f41b8a141c7a01618fed005462f6
SHohruhMamatov <145185852+SHohruhMamatov@users.noreply.github.com>
2024-09-20 07:48:05 +0500

    Update Homework1.java

--- student/Homework1.java
+++ student/Homework1.java
@@ -2,28 +2,55 @@ package student;
 
 public class Homework1 {
 
-    public double shippingCalculator(int n){
-        //TODO Write your code here for problem 1
-        return 0;
+   public double shippingCalculator(int n){
+        return 10.95+(n-1)*2.95;
     }
     public boolean isValidTriangle(int a, int b, int c){
-        //TODO Write your code here for problem 2
-        return false;
+        if(a+b>c && a+c>b && b+c>a){
+            return true;
+        }else {
+            return false;
+        }
+
 
     }
     public boolean isPrime(int number){
-        //TODO Write your code here for problem 3
-        return false;
+        boolean isP = false;
+        for (int i = 2; i < (number); i++) {
+            if (number % i != 0) {
+               isP=true;
+            }
+        }
+        return isP;
     }
 
-    public  int hex2int(String hex) {
-        //TODO Write your code here for problem 4
+    public  static int hex2int(String hex) {
+        hex=hex.toUpperCase();
+        if (hex.length()>1){
+            return -1;
+        }else if (hex.matches("[A-F]")){
+            return hex.charAt(0)-'A'+10;
+        }
+        if (hex.matches("[0-9]")){
+            return Integer.parseInt(hex);
+        }
         return 0;
     }
     public String int2hex(int value) {
-        //TODO Write your code here for problem 4
-        return "";
+        if (value < 0 && value > 15) {
+            return "Invalid value";
+        }else if (value<=9) {
+            return String.valueOf(value);
+        }else{
+            return String.valueOf((char)('A' + value - 10));
+        }
+
     }
 
 
 }
+
+    
+
+
+
